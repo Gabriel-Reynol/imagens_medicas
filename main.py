4f94bbd70d8a523a631c4d3a4efc2443766f0c87
Gabriel-Reynol <reynolgabriel@usp.br>
2026-03-24 15:05:34 -0300

    usando a base nova

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import fase3_server_dl
 
 
 # --- 1. DETECÇÃO DE AMBIENTE ---
-SERVER_PATH = '/Storage/jerogalsky-2024'
+SERVER_PATH = '/Storage/jerogalsky-2025'
 PC_PATH = 'C:/Users/reyno/USP2025/pacientes/'
 
 if os.path.exists(SERVER_PATH):
@@ -16,8 +16,8 @@ if os.path.exists(SERVER_PATH):
     CAMINHO_IMG = SERVER_PATH
 
     PASTA_CSVS = '/home/jerogalsky/tabelasSeparadas'
-    CAMINHO_CSV_COM = os.path.join(PASTA_CSVS, 'planilha_uid_contraste.csv')
-    CAMINHO_CSV_SEM = os.path.join(PASTA_CSVS, 'planilha_uid_SC.csv')
+    CAMINHO_CSV_COM = os.path.join(PASTA_CSVS, 'hc-2025_exames_contraste_novo.csv')
+    CAMINHO_CSV_SEM = os.path.join(PASTA_CSVS, 'hc-2025_exames_semcontraste_novo.csv')
 
     EPOCHS = 30
     BATCH_SIZE = 16
